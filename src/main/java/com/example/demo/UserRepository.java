5b8b0f3f2eebb4e273d1eb5e7bd0a2d87a9bf63d
kksun <657910416@qq.com>
2019-03-21 04:31:13 -0400

    demo of login system

---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    List<User> findByEmail(String email);
+    void deleteByEmail(String email);
+}
